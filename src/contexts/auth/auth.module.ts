22e43f6db115f60a4670f271b556ed72e701a3e3
wilton <wiltonsousajr@hotmail.com>
2025-07-11 19:20:07 -0300

    feat: auth module

---
+++ src/contexts/auth/auth.module.ts
@@ -0,0 +1,24 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './application/services/auth.service';
+import { AuthController } from './presentation/controllers/auth.controller';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { UserModule } from '../user/users.module';
+
+@Module({
+  imports: [
+    UserModule,
+    ConfigModule,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get<string>('SECRETKEY'),
+        signOptions: { expiresIn: config.get<string>('EXPIRESIN') },
+      }),
+    }),
+  ],
+  providers: [AuthService],
+  controllers: [AuthController],
+})
+export class AuthModule { }
