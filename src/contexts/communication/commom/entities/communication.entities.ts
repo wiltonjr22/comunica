3b4b2de6a0d3bf9adcc0e06471bd5a0cbd09d757
wilton <wiltonsousajr@hotmail.com>
2025-07-11 13:43:25 -0300

    refa: for abstract factory pattern

--- src/contexts/communication/commom/entities/communication.entities.ts
+++ src/contexts/communication/commom/entities/communication.entities.ts
@@ -12,44 +12,17 @@ export enum StatusTipo {
   SENT = "enviado",
 }
 
-export class CommunicationFactoryEntity {
-  @Expose({ name: "titulo" })
-  @IsString()
+export abstract class CommunicationFactoryEntity {
   titulo: string;
-
-  @Expose({ name: "conteudo" })
-  @IsString()
   conteudo: string;
-
-  @Expose({ name: "tipo_canal" })
-  @IsEnum(CanalTipo)
   tipo_canal: CanalTipo;
-
-  @Expose({ name: "status" })
-  @IsEnum(StatusTipo)
   status: StatusTipo;
-
-  @Expose({ name: "autor" })
-  @IsString()
   autor: string;
-
-  @Expose({ name: "data_criacao" })
-  @IsDate()
   data_criacao: Date;
-
-  @Expose({ name: "deletado_em" })
-  @IsOptional()
-  @IsDate()
   deletado_em?: Date;
-
-  @Expose({ name: "data_envio" })
-  @IsOptional()
-  @IsDate()
   data_envio?: Date;
 }
 
 export class CommunicationEntity extends CommunicationFactoryEntity {
-  @Expose({ name: "id" })
-  @IsUUID()
   id: string;
 }
